7764485e2bb55c7232b9d5d4c1ab9a9c1cc4fa65
sabbir1257 <masabbir1257@gmail.com>
2023-03-27 03:31:11 +0600

    header section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
