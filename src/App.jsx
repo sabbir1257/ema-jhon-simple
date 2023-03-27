7a0ee598f2b2114100520bf8d916ffabe174af08
sabbir1257 <masabbir1257@gmail.com>
2023-03-27 21:00:53 +0600

    cart section push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
