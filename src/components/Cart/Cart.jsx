401e8eb61ec558f2f1ab3c0f0e257f63381436df
sabbir1257 <masabbir1257@gmail.com>
2023-03-30 06:15:42 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,9 +9,17 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //   product.quantity = 1;
+        // }
+
+        // product.quantity = product.quantity || 1;
+  
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tex = totalPrice*7/100;
@@ -21,7 +29,7 @@ const Cart = ({cart}) => {
   return (
     <div className="cart">
       <h3>Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tex.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -28,7 +28,7 @@ const Shop = () => {
                 // step-4: add the added product to the save cart 
                 saveCart.push(addedProduct);
             }
-            console.log(addedProduct);
+            // console.log(addedProduct);
         }
         // step-5: set the cart 
         setCart(saveCart);
@@ -36,7 +36,19 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product)
-        const newCart = [...cart, product];
+        let newCart = [];
+        // if product doesn't exist in the cart, then set quantity = 1;
+        // if exist update quantity by 1 
+        const exists =  cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists]; 
+        }
         setCart(newCart);
         addToDb(product.id)
     }
